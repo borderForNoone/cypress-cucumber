a25ff5056371ca2e527d3596b81942cc26b04eb0
DenysKulikov <deniska2002kulikov@gmail.com>
2024-07-25 11:01:06 +0300

    changed yml

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,46 +1,40 @@
-import { defineConfig } from "cypress";
-import webpack from "@cypress/webpack-preprocessor";
-import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
-import allureWriter from "@shelex/cypress-allure-plugin/writer";
+import { defineConfig } from 'cypress';
+import webpack from '@cypress/webpack-preprocessor';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import allureWriter from '@shelex/cypress-allure-plugin/writer';
 
-async function setupNodeEvents(
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
-): Promise<Cypress.PluginConfigOptions> {
+async function setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions): Promise<Cypress.PluginConfigOptions> {
   await addCucumberPreprocessorPlugin(on, config);
 
-  on(
-    "file:preprocessor",
-    webpack({
-      webpackOptions: {
-        resolve: {
-          extensions: [".ts", ".js"],
-        },
-        module: {
-          rules: [
-            {
-              test: /\.ts$/,
-              exclude: [/node_modules/],
-              use: [
-                {
-                  loader: "ts-loader",
-                },
-              ],
-            },
-            {
-              test: /\.feature$/,
-              use: [
-                {
-                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
-                  options: config,
-                },
-              ],
-            },
-          ],
-        },
+  on('file:preprocessor', webpack({
+    webpackOptions: {
+      resolve: {
+        extensions: ['.ts', '.js'],
       },
-    })
-  );
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: [/node_modules/],
+            use: [
+              {
+                loader: 'ts-loader',
+              },
+            ],
+          },
+          {
+            test: /\.feature$/,
+            use: [
+              {
+                loader: '@badeball/cypress-cucumber-preprocessor/webpack',
+                options: config,
+              },
+            ],
+          },
+        ],
+      },
+    },
+  }));
 
   allureWriter(on, config);
 
@@ -49,12 +43,9 @@ async function setupNodeEvents(
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://conduit.realworld.how/",
-    specPattern: "**/*.feature",
+    baseUrl: 'https://conduit.realworld.how/',
+    specPattern: '**/*.feature',
     setupNodeEvents,
     pageLoadTimeout: 6000,
-    env: {
-      allureReuseAfterSpec: true,
-    },
   },
 });
